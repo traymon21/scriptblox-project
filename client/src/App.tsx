3a1f76844394701bd0dfccd26b83fcb397fbbd27
traymonpro <mnizamshaka291@gmail.com>
2025-12-25 17:03:15 +0700

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import ScriptDetail from "./pages/ScriptDetail";
+import AdminPanel from "./pages/AdminPanel";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/script/:slug" component={ScriptDetail} />
+      <Route path="/admin" component={AdminPanel} />
+      <Route path="/404" component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
